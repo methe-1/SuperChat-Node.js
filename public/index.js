688a0e8215da7ebbff20b4bc21801634abaad151
methe ezzitouni <methe@walo.app>
2022-09-29 00:46:11 +0100

    enhanced the superChat

--- public/index.js
+++ public/index.js
@@ -1,37 +1,72 @@
 var socket = io.connect("http://localhost:8080");
-var pseudo = prompt("donnez votre pseudo");
 
-socket.emit("new_user", pseudo);
-document.title = pseudo + ": " + document.title;
 
-socket.on("nbr_users", function (data) {
+
+socket.on("actives", function (data) {
   $("#nbr_users").html(data);
 });
 
-socket.on("new_user", function (pseudo) {
-  $("#conversation").prepend("<p><em> " + pseudo + " is here </em></p>");
+// socket.on("new_user", function (pseudo) {
+//   $("#conversation").append(`
+//     <div  class="message_container  ">
+//         ${pseudo} just got online!
+//     </div>
+//   `);
+// });
+// socket.on("welcome", (pseudo) => {
+//   $("#conversation").append(`
+//     <div  class="message_container message_container_mb  ">
+//         Welcome ${pseudo}!
+//     </div>
+//   `);
+
+// })
+socket.on("user_left", function (pseudo) {
+    $("#conversation").prepend("<p><em> " + pseudo + " has left </em></p>");
 });
+
+socket.on("msg", function (data) {
+    $(".conversation_container").append(msgcontainer(data.pseudo, data.message, false));
+    $("#conversation").animate({ scrollTop:  $('#conversation').prop('scrollHeight')}, 50);
+});
+
+var pseudo = prompt("donnez votre pseudo") || 'anonymous';
+document.title = `${document.title}: ${pseudo}`;
+
+socket.emit("new_user", pseudo);
 
 $("#send").submit(function () {
   message = $("#msgcontent").val();
-  socket.emit("msg", message);
-  $("#conversation").prepend(msgcontainer(pseudo, message));
+  socket.emit("msg", { pseudo, message});
+  $(".conversation_container").append(msgcontainer(pseudo, message, true));
+  $("#conversation").animate({ scrollTop:  $('#conversation').prop('scrollHeight')}, 50);
   message = $("#msgcontent").val("").focus();
   return false;
 });
 
-socket.on("msg", function (data) {
-  $("#conversation").prepend(msgcontainer(data.pseudo, data.message));
-});
 
-function msgcontainer(pseudo, msg) {
+
+$(window).unload(function(){
+    socket.emit("quit", pseudo);
+
+ });
+function msgcontainer(pseudo, msg, mine) {
   return (
-    '<p> <h5 style="display:inline">' +
-    pseudo +
-    " " +
-    "</h5>" +
-    "<span>" +
-    msg +
-    "</span></p>"
+    `
+    <div  class="message_container ${ mine ? 'message_container_mb' : ''}  ">
+        <div style="width: 100%;">
+            <div class="msg_sub_cont ${ mine ? 'my_message' : 'their_message'}">
+                <div class="message ${ mine ? 'my_msg_color' : 'their_msg_color'}">
+                ${msg}
+                </div>
+            </div>
+            <div style="display: flex; justify-content: flex-end; padding-right: 15px; ">
+                <sub style="font-size: smaller; vertical-align: sub; ">
+                ${mine ? '' : pseudo}
+                </sub>
+            </div>
+        </div>
+    </div>
+    `
   );
 }
