50ec863d82de221cec2d2be0e8fe4e986e310c2b
methe ezzitouni <methe@walo.app>
2022-10-16 16:57:34 +0100

    added authentication, and message storage/ firestore

--- index.js
+++ index.js
@@ -3,95 +3,142 @@ const express = require("express");
 const ent = require("ent");
 const socket = require("socket.io");
 
-const db = require('./firebase.js');
-
-
-// const { db, ACTIVES,  USERS } = require('./db');
-
+const { FieldValue } = require('firebase-admin/firestore');
+const db = require("./firebase.js");
+const { errorLogger, logger } = require("./utils/logger");
 
 // set up server and socket.io
 const app = express();
 const server = http.createServer(app);
 const io = socket.listen(server);
-
 // serve the frontend
-app.use('/', express.static('public'));
+app.use("/", express.static("public"));
 
+// logger
+app.use(logger());
+// body parser
 app.use(express.json());
-app.post('/auth', (req, res) => {
-  console.log('====================================');
-  console.log('body', req.body);
-  console.log('====================================');
-  res
-  .status(200)
-  .send({ body: req.body})
-})
-io.sockets.on("connection", async function (socket) {
-  
-  
-  socket.on("connected_user", async function () {
 
-    // const existed_users = JSON.parse(db.getItem(USERS));
+// auth endpoint
+app.post("/auth", async (req, res) => {
+  const { body } = req;
+  try {
+    let user = null;
+    const check_username = await db
+      .collection("users")
+      .select("username", "connected", "email")
+      .where("username", "==", body.username)
+      .get();
+   
+      if(!check_username.docs.length ){
+        user = await db
+              .collection('users')
+              .add({ username: ent.encode(body.username), password: ent.encode(body.password), connected: true })
+        user = await user.get();
+        user.data = user.data();
+      }else{
+        user = await db
+              .collection("users")
+              .select("username", "connected", "email")
+              .where("username", "==", body.username)
+              .where("password", "==", body.password)
+              .get();
+        if (!user.docs.length) throw { code: 409, details: "If you're singing up then this username is taken, otherwise if if you're logging in, then your password is incorrect" };
+        // set user.connected to be true
+        await db.collection('users').doc(user.docs[0].id).update({
+          connected: true
+        })
+        user = { id: user.docs[0].id, data: user.docs[0].data()}
+      }
 
-    // socket.emit("start_auth");
-    
-    socket.on('add_user', async (pseudo) => {
-
-      // remove this
-      db.setItem(ACTIVES, Number(db.getItem(ACTIVES)) + 1);
       
-      // create user 
-      // const user = await db.collection('users').add({ username: ent.encode(pseudo), connected: true })
-      db.setItem(USERS, JSON.stringify([...existed_users, pseudo]));
-
-      // get all active users
-      // const users = await db.collection('users').where('connected', '==', true).get();
-      
-      
-      // broadcast to the new user
-      // socket.emit("welcome", user);
-      socket.emit("welcome", ent.encode(pseudo || ''));
-      
-      // socket.emit('actives', users);
-      socket.emit('actives', Number(db.getItem(ACTIVES)));
-      
-      
-      // broadcast to eveyone except the new user
-      // socket.broadcast.emit("new_user", user);
-      socket.broadcast.emit("new_user", ent.encode(pseudo || ''));
-      
-      // socket.broadcast.emit('actives', users);
-      socket.broadcast.emit("actives", Number(db.getItem(ACTIVES)));
-
-      socket.on("msg", async (data) => {
-        // added msg to db
-        // const msg = await db.collection('messages').add({ body: ent.encode(data.message), date: FieldValue.serverTimestamp(), user_id: `/users/${data.user.id}`})
-        //  socket.broadcast.emit("msg", { pseudo: data.user, message: msg.body });
-        socket.broadcast.emit("msg", { pseudo: data.pseudo, message: ent.encode(data.message) });
-      });
-
-      socket.on("quit", async (pseudo) => {
-        // update DB
-        // await db.collection('users').doc(user.id).update({ connected: false });
-        db.setItem(ACTIVES, Number(db.getItem(ACTIVES)) - 1);
-        
-        // broadcast a user left 
-        // socket.broadcast.emit("user_left", user);
-
-        // remove user
-        let users = JSON.parse(db.getItem(USERS));
-        db.setItem(USERS, JSON.stringify(users.splice(users.indexOf(pseudo), 1)));
-        
-        socket.broadcast.emit("actives", Number(db.getItem(ACTIVES)));
-        socket.broadcast.emit("user_left", ent.encode(pseudo));
-      })
-    })
-
-    
-    
-  });
+    res.status(200).send({ id: user.id, ...user.data });
+  } catch (error) {
+    console.log('====================================');
+    console.log(error);
+    console.log('====================================');
+    req.error = error;
+    res.status(error.code || 500).send({
+      details: error.details || JSON.stringify("something went wrong!"),
+    });
+  }
 });
 
+// socket
+io.sockets.on("connection", async function (socket) {
 
+  
+  try {
 
-server.listen(process.env.PORT || 3001)
+    socket.on("verify_user", async function (user) {
+   
+      try {
+        const userRef = await db.collection("users").doc(user.id);
+
+        socket.emit("verified", true);
+    
+        // get all active users
+        const { docs: users } = await db
+        .collection("users")
+        .select("username", "email", "connected")
+        .where("connected", "==", true)
+        .get();
+    
+        // broadcast to the new user
+        socket.emit("welcome", user);
+      
+        socket.emit("actives", mapUsers(users));
+        
+        // broadcast to eveyone except the new user
+
+        socket.broadcast.emit("new_user", user);
+
+        // socket.broadcast.emit("actives", mapUsers(users));
+
+      } catch (error) {
+        errorLogger(error);
+        socket.emit("verified", false);
+      }
+    });
+    
+
+    socket.on("msg", async (data) => {
+      // added msg to db
+      try {
+        const userRef = await db.collection("users").doc(data.user.id);
+
+        let msg = await db.collection("messages").add({
+          body: ent.encode(data.message),
+          date: FieldValue.serverTimestamp(),
+          user_id: `/users/${data.user.id}`,
+        });
+        msg = await msg.get();
+
+        socket.broadcast.emit("msg", {
+          user: data.user,
+          message: msg.data().body,
+        });
+      } catch (error) {
+        socket.emit("mesg", {
+          user: data.user,
+          msg: "send message failed!"
+        })
+      }
+    });
+
+    socket.on("quit", async (user) => {
+    // update DB
+    await db.collection('users').doc(user.id).update({ connected: false });
+
+    // broadcast a user left
+    socket.broadcast.emit("user_left", {...user, connected: false});
+  });
+  } catch (error) {
+    errorLogger(error);
+    socket.emit("verified", false);
+  }
+});
+function mapUsers(users){
+  return users.map(user => ({ id: user.id, ...user.data() }))
+}
+server.listen(process.env.PORT || 3001);
--- public/index.js
+++ public/index.js
@@ -1,95 +1,213 @@
 const host = 'http://localhost:3001' // "https://superchat.onrender.com"
-var socket = io.connect();
-var message_vanish_id = null;
+var socket = io.connect(host);
 
 
-socket.on("actives", function (data) {
-  $("#nbr_users").html(data);
-});
-
-socket.on("new_user", function (pseudo) {
-  $("#socket_message").html(`${pseudo} just got online!`);
-
-  if(message_vanish_id) clearTimeout(message_vanish_id);
-  message_vanish_id = setTimeout(() => $("#socket_message").html(''), 4 * 1000)
-});
-socket.on("welcome", (pseudo) => {
-  $("#socket_message").html(`Welcome ${pseudo}!`);
-
-  if(message_vanish_id) clearTimeout(message_vanish_id);
-  message_vanish_id = setTimeout(() => $("#socket_message").html(''), 4 * 1000)
-})
-socket.on("user_left", function (pseudo) {
-    $("#socket_message").html(`${pseudo} has left!`);
-
-    if(message_vanish_id) clearTimeout(message_vanish_id);
-    message_vanish_id = setTimeout(() => $("#socket_message").html(''), 4 * 1000)
-});
-
-socket.on("msg", function (data) {
-    $(".conversation_container").append(msgcontainer(data.pseudo, data.message, false));
-    $("#conversation").animate({ scrollTop:  $('#conversation').prop('scrollHeight')}, 50);
-});
+auth();
 
 
-let users = []
-let pseudo = null;
 
-socket.on("receive_users", function (data) {
-    users = data;
-    let prompt_msg = "type your username"
-    while(!pseudo) {
-        pseudo = prompt(prompt_msg);
-        
-        if(pseudo.length < 3){
-            prompt_msg = `${pseudo}: username must be at least of 3 charcaters`;
-            pseudo = null;
-        }
+function auth(){
 
-        if(users.indexOf(pseudo) != -1){
-            prompt_msg = `${pseudo} is taken`;
-            pseudo = null;
-        }
-        
+    async function authenticate(data){
+        return await axios.post(`${host}/auth`, data)
     }
-    document.title = `${document.title}: ${pseudo}`;
-    socket.emit('add_user', pseudo);
 
-   
-});
+    var dialog, form,
+    
+        // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
+        // emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+        name = $( "#name" ),
+        // email = $( "#email" ),
+        password = $( "#password" ),
+        allFields = $( [] ).add( name ).add( password ),
+        tips = $( ".validateTips" );
+    
+    function updateTips( t ) {
+        tips
+        .text( t )
+        .addClass( "ui-state-highlight" );
+        setTimeout(function() {
+        tips.removeClass( "ui-state-highlight", 1500 );
+        }, 500 );
+    }
+    
+    function checkLength( o, n, min, max ) {
+        if ( o.val().length > max || o.val().length < min ) {
+        o.addClass( "ui-state-error" );
+        updateTips( "Length of " + n + " must be between " +
+            min + " and " + max + "." );
+        return false;
+        } else {
+        return true;
+        }
+    }
+    
+    function checkRegexp( o, regexp, n ) {
+        if ( !( regexp.test( o.val() ) ) ) {
+        o.addClass( "ui-state-error" );
+        updateTips( n );
+        return false;
+        } else {
+        return true;
+        }
+    }
+    
+    async function addUser() {
+        var valid = true;
+        allFields.removeClass( "ui-state-error" );
+    
+        valid = valid && checkLength( name, "username", 3, 16 );
+        // valid = valid && checkLength( email, "email", 6, 80 );
+        valid = valid && checkLength( password, "password", 5, 16 );
+    
+        valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+        // valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+        valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+    
+        if ( valid ) {
+        try {
+            const res = await authenticate({ username: name.val(), password: password.val() })
+            dialog.dialog( "close" );
+            $('.container').addClass('container-show');
+            setupSocketEvents(res.data);
+            setupMessageEmitting(res.data);
+            setupUserQuitEventEmitting(res.data);
+        } catch (error) {
+            let status = error.response && error.response.status || 0;
+            console.log('====================================');
+            console.log(error);
+            console.log('====================================');
+            updateTips(status >= 400 && status <= 499 ? error.response.data.details : 'email/password is wrong!')
+        }
+        }
+        return valid;
+    }
+    
+    dialog = $( "#dialog-form" ).dialog({
+        autoOpen: false,
+        height: 350,
+        width: 350,
+        modal: true,
+        buttons: {
+        "Login/signup": addUser,
+        // Cancel: function() {
+        //     dialog.dialog( "close" );
+        // }
+        },
+        close: function() {
+        form[ 0 ].reset();
+        allFields.removeClass( "ui-state-error" );
+        }
+    });
+    
+    form = dialog.find( "form" ).on( "submit", function( event ) {
+        event.preventDefault();
+        addUser();
+    });
+    
+    // $( "#create-user" ).button().on( "click", function() {
+    //  
+    // });
 
-$("#send").submit(function () {
-    message = $("#msgcontent").val();
-    socket.emit("msg", { pseudo, message});
-    $(".conversation_container").append(msgcontainer(pseudo, message, true));
-    $("#conversation").animate({ scrollTop:  $('#conversation').prop('scrollHeight')}, 50);
-    message = $("#msgcontent").val("").focus();
-    return false;
-});
+    dialog.dialog( "open" );
+    // document.title = `${document.title}: ${pseudo}`;
+    // socket.emit('add_user', pseudo);
+}
 
-$(window).unload(function(){
-    socket.emit("quit", pseudo);
-});
+function setupMessageEmitting(user){
+    $("#send").submit(function () {
+        message = $("#msgcontent").val();
+        socket.emit("msg", { user, message });
+        $(".conversation_container").append(msgcontainer(null, message, true));
+        $("#conversation").animate({ scrollTop:  $('#conversation').prop('scrollHeight')}, 50);
+        message = $("#msgcontent").val("").focus();
+        return false;
+    });
+}
 
-socket.emit("connected_user");
+function setupUserQuitEventEmitting(user){
+    $(window).on('unload', function(){
+        socket.emit("quit", user);
+    });
+}
+
+function setupSocketEvents(user){
+    let message_vanish_id = null;
+    let users = null;
+
+    socket.emit('verify_user', user);
+    socket.on('verified', (verified) => {
+        if(verified){
+            socket.on("welcome", (user) => {
+                $("#socket_message").html(`Welcome ${user.username}!`);
+                
+                if(message_vanish_id) clearTimeout(message_vanish_id);
+                message_vanish_id = setTimeout(() => $("#socket_message").html(''), 4 * 1000)
+            })
+
+            socket.on("actives", function (users_list) {
+                users = users_list;
+
+                $("#nbr_users").html(users_list.length);
+            });
+            
+            socket.on("new_user", function (user) {
+                // add user to active users
+                if(!users.find((active => active.id == user.id))){
+                    users.push(user);
+                    $("#nbr_users").html(users.length);
+                }
+                
+                $("#socket_message").html(`${user.username} just got online!`);
+                
+                if(message_vanish_id) clearTimeout(message_vanish_id);
+                message_vanish_id = setTimeout(() => $("#socket_message").html(''), 4 * 1000)
+            });
+           
+            socket.on("user_left", function (left) {
+                if(user.id == left.id) {
+                    location.reload();
+                }
+                $("#socket_message").html(`${left.username} has left!`);
+                
+                users = users.filter(active => active.id != left.id);
+
+                // update active user's number 
+                $("#nbr_users").html(users.length);
+
+                // notify user
+                if(message_vanish_id) clearTimeout(message_vanish_id);
+                message_vanish_id = setTimeout(() => $("#socket_message").html(''), 4 * 1000)
+            });
+            
+            socket.on("msg", function (data) {
+                $(".conversation_container").append(msgcontainer(data.user.username, data.message, false));
+                $("#conversation").animate({ scrollTop:  $('#conversation').prop('scrollHeight')}, 50);
+            });
+            
+            socket.emit('fetch');
+        }
+    })
+}
+
 
 function msgcontainer(pseudo, msg, mine) {
-  return (
-    `
-    <div  class="message_container ${ mine ? 'message_container_mb' : ''}  ">
-        <div style="width: 100%;">
-            <div class="msg_sub_cont ${ mine ? 'my_message' : 'their_message'}">
-                <div class="message ${ mine ? 'my_msg_color' : 'their_msg_color'}">
-                ${msg}
+    return (
+      `
+        <div  class="message_container ${ mine ? 'message_container_mb' : ''}  ">
+            <div style="width: 100%;">
+                <div class="msg_sub_cont ${ mine ? 'my_message' : 'their_message'}">
+                    <div class="message ${ mine ? 'my_msg_color' : 'their_msg_color'}">
+                    ${msg}
+                    </div>
+                </div>
+                <div style="display: flex; justify-content: flex-end; padding-right: 15px; ">
+                    <sub style="font-size: smaller; vertical-align: sub; ">
+                    ${mine ? '' : pseudo}
+                    </sub>
                 </div>
             </div>
-            <div style="display: flex; justify-content: flex-end; padding-right: 15px; ">
-                <sub style="font-size: smaller; vertical-align: sub; ">
-                ${mine ? '' : pseudo}
-                </sub>
-            </div>
         </div>
-    </div>
-    `
-  );
-}
+      `
+    );
+  }
